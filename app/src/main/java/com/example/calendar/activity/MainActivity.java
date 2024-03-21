427665cf412a3c583819f538916bec31c78ece13
LaSeineHuang <91377231+LaSeineHuang@users.noreply.github.com>
2024-03-21 23:00:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/calendar/activity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.calendar.activity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.calendar.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tvVersion = (TextView) findViewById(R.id.tv_version);
+
+
+    }
+
+
+}
